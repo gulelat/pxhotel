6581fdad560df6fb21d1b7ec587fd1b0c2e1858c
levanvunam@gmail.com <levanvunam@gmail.com@10f058b1-af60-83b3-81cf-68cf1259b6e1>
2014-03-03 18:41:19 +0000

    - Update simple admin master page

---
+++ Hotel/trunk/PX.EntityModel/Enums/UserEnums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.EntityModel.Enums
+{
+    public class UserEnums
+    {
+        public enum UserTypesEnums
+        {
+            Admin = 1,
+            Moderator = 2,
+            CameraManager = 6,
+            SchoolManager = 3,
+            Teacher = 4,
+            Parent = 5
+        }
+
+
+        public enum UserStatusEnums
+        {
+            Active = StatusEnums.UserActive,
+            Inactive = StatusEnums.UserInactive,
+            Locked = StatusEnums.UserLocked
+        }
+    }
+}
